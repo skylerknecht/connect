f169adbce09854f943685ce8441bbead02ba68ed
Skyler Knecht <skyler.knecht@outlook.com>
2021-06-21 01:43:12 -0400

    Added proper escapes to the colors to prevent issues with readline.

--- connect/color.py
+++ connect/color.py
@@ -1,11 +1,11 @@
 LEVEL_COLORS={
-    'cyan':'\033[0;36m',
-    'green':'\033[0;32m',
-    'red':'\033[0;31m',
-    'reset':'\033[0;0m',
+    'cyan':'\001\033[0;36m\002',
+    'green':'\001\033[0;32m\002',
+    'red':'\001\033[0;31m\002',
+    'reset':'\001\033[0;0m\002',
 }
 
-def display_terminal(message):
+def display_prompt(message):
     terminal_color = LEVEL_COLORS['cyan']
     reset = LEVEL_COLORS['reset']
     return input(f'{terminal_color}{message}{reset} ')
