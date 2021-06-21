be519f368c456019e13ad82453ec70f1fdd1bcdf
Skyler Knecht <skyler.knecht@outlook.com>
2021-06-21 08:01:55 -0400

    First draft of the server and assisting initilizaiton changes.

--- connect/__init__.py
+++ connect/__init__.py
@@ -1,7 +1,15 @@
-from connect import cli, engine
+from . import cli, engine
+import threading
+import time
 
-__version__ = '0.1'
+__version__ = '0.3'
 
 def run(ip, port):
-    connect_server = engine.Server(ip, port)
-    return cli.run(connect_server)
+    connect_engine = threading.Thread(target=engine.run,args=(ip, port))
+    connect_engine.daemon = True
+    connect_engine.start()
+    cli.display_banner()
+    color.warning('Server Information:')
+    time.sleep(.1)
+    cli.run()
+    return
--- connect/engine.py
+++ connect/engine.py
@@ -1,38 +1,84 @@
-import socket
+import connect.color as color
+import os
+import random
+import time
 
-from connect import color
+from flask import Flask, render_template
 
-class Server:
+app = Flask(__name__)
+connections = {}
+titles = ['The connections you build over time.', 'Everything is connected.', 'Unlock the mind to connect to the universe of thought.']
 
-    data = b''
+class Connection():
 
-    def __init__(self, ip, port):
-        self.ip = ip
-        self.port = port
-        self.create_socket()
+    implant_data = ''
 
-    def create_socket(self):
-        self.server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.server.bind((self.ip, int(self.port)))
-        color.success(f'Socket successfully created. ({self.ip}:{self.port})')
-        return 0
-
-    def receive_data(self):
-        self.data = self.data + self.server.recv(60000)
-        return 0
-
-    def data_size(self):
-        color.normal(len(self.data))
-        return 0
-
-class Receiver:
-
-    def __init__(self, data, name):
-        self.data = data
-        self.name = name
+    def __init__(self):
+        self.connection_id = connection_id()
+        self.last_checkin = '0:0:0'
+        self.original_checkin = Connection.get_current_time()
+        self.status = 'Waiting'
+        self.type = ''
 
     def __str__(self):
-        color.normal(f'{self.name} : {len(self.data)}')
+        return f'Original Checkin {self.original_checkin} || Last Checkin: {self.last_checkin} || Status: {self.status} || Type: {self.type}'
 
-class DiscordReceiver(Receiver):
-    pass
+    def update_checkin(self):
+        self.last_checkin = self.get_current_time()
+
+    def return_implant(self):
+        return self.implant_data
+
+    @staticmethod
+    def get_current_time():
+        return time.strftime("%H:%M:%S", time.localtime())
+
+    @staticmethod
+    def mshta_connection():
+        return 'yup pwned'
+
+class MshtaConnection(Connection):
+
+    implant_data = 'pwned brah'
+
+    def __init__(self):
+        super().__init__()
+        self.type = 'mshta'
+
+def connection_id():
+    new_connection_id = [str(random.randint(0,9)) for random_integer in range (0,10)]
+    new_connection_id = ''.join(new_connection_id)
+    if new_connection_id in connections.keys():
+        new_connection_id = connection_id()
+    return new_connection_id
+
+def display_connections():
+    color.display_banner('Current Connections')
+    if not connections.items():
+        color.normal('No connections to display.\n')
+        return 0
+    for connection_id, connection in connections.items():
+        if connection.status == 'Success':
+            color.success(f'\tConnection ID: {connection_id} || {connection}')
+        color.normal(f'Connection ID: {connection_id} || {connection}')
+    color.normal('')
+    return 0
+
+@app.route('/<type>', methods=['GET'])
+def serve_connection(type):
+    connection_types = {'mshta':MshtaConnection()}
+    if type not in connection_types.keys():
+        return render_template(connection_template, title=Titles[random.randint(0,2)], data='')
+    new_connection_id = connection_id()
+    connections[new_connection_id] = connection_types[type]
+    return connections[new_connection_id].return_implant()
+
+@app.route('/connection/<int:connection_id>', methods=['POST'])
+def new_connection(connection_id):
+    #if connection_id not in connections.keys():
+    #    abort(400, 'Not Found')
+    connections[connection_id].update_checkin()
+
+def run(ip, port):
+    os.environ['WERKZEUG_RUN_MAIN'] = 'true'
+    app.run(host=ip, port=int(port))
