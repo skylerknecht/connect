2a81a4d972a3d12beb5537c454a2b41e418c4750
Skyler <Skyler.Knecht@outlook.com>
2022-05-16 09:55:06 -0400

    Execute assembly now uses xor and base64

--- connect/client/cli/command_sets/execution.py
+++ connect/client/cli/command_sets/execution.py
@@ -1,6 +1,6 @@
 import argparse
 
-from connect.convert import string_to_base64, bytes_to_base64
+from connect.convert import string_to_base64, bytes_to_base64, xor_base64
 from cmd2 import Cmd, Cmd2ArgumentParser, CommandSet, with_argparser, with_default_category
 
 
@@ -39,8 +39,8 @@ class ExecutionCommands(CommandSet):
     def do_execute_assembly(self, args: argparse.Namespace):
         """ Executes a .NET assembly with reflection """
         with open(args.assembly, 'rb') as fd:
-            file = bytes_to_base64(fd.read())
+            key, file = xor_base64(fd.read())
         arg_str = ''
         for arg in args.args:
             arg_str = arg_str + ',' + string_to_base64(arg) + 'utf-8'
-        self.post_job(f'"name":"execute_assembly","arguments":"{file}{arg_str}","type":1')
+        self.post_job(f'"name":"execute_assembly","arguments":"{key},{file}{arg_str}","type":1')
