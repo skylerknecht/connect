a83429b3346d48f801f973104a5d0ce2630dc123
skylerknecht <skyler.knecht@outlook.com>
2023-09-12 11:11:19 -0400

    Updated the type command

--- connect/cli/commands/type.py
+++ connect/cli/commands/type.py
@@ -1,22 +1,32 @@
 import json
 
 from .commands import FileSystemCommand
+from connect.convert import string_to_base64
 
 
 class Type(FileSystemCommand):
     def __init__(self):
         super().__init__(
             'type',
-            'Retrieve the contents of a file'
+            'Retrieve the contents of a file',
+            parameters={
+                'file': 'What file contents should we retrieve'
+            }
         )
 
     def execute_command(self, parameters, current_agent, client_sio):
+        if len(parameters) != 1:
+            self.help()
+            return
         type_task = {
             'create': {
                 'agent': current_agent,
                 'method': self.name,
                 'type': 0,
                 'module': self.module,
+                'parameters': [
+                    string_to_base64(parameters[0])
+                ]
             }
         }
         client_sio.emit('task', json.dumps(type_task))
